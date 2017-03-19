6afc3b8c76e3e5dd1603cdfd19707c35997000e0
Nick Ebbitt <nickebbitt@gmail.com>
2017-03-19 21:53:53 +0000

    Simplfy logging output

--- src/main/java/com/nickebbitt/Application.java
+++ src/main/java/com/nickebbitt/Application.java
@@ -9,40 +9,68 @@ import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.context.request.async.DeferredResult;
 
 import java.util.concurrent.CompletableFuture;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
 
 @SpringBootApplication
 @RestController
 @Slf4j
 public class Application {
 
-    public static final String ASYNC_RESULT = "Async Result";
+    public static final String RESULT = "Result";
 
     public static void main(String[] args) {
 		SpringApplication.run(Application.class, args);
 	}
 
-	@RequestMapping(path = "/async", method = RequestMethod.GET)
-	public CompletableFuture<String> getValueAsyncUsingCompletableFuture() {
-
-	    log.info("Request received: {} : {}", Thread.currentThread().getId(), Thread.currentThread().getName());
-
-        CompletableFuture<String> stringCompletableFuture = CompletableFuture
-                .supplyAsync(this::processRequest);
-
-        log.info("Servlet thread released: {} : {}", Thread.currentThread().getId(), Thread.currentThread().getName());
-
-		return stringCompletableFuture;
-
-	}
-
     private String processRequest() {
-        log.info("Processing request: {} : {}", Thread.currentThread().getId(), Thread.currentThread().getName());
+        log.info("Start processing request");
         try {
             Thread.sleep(1000);
         } catch (InterruptedException e) {
             e.printStackTrace();
         }
-        log.info("Completed processing request: {} : {}", Thread.currentThread().getId(), Thread.currentThread().getName());
-        return ASYNC_RESULT;
+        log.info("Completed processing request");
+        return RESULT;
     }
+
+    @RequestMapping(path = "/sync", method = RequestMethod.GET)
+    public String getValueSync() {
+
+        log.info("Request received");
+
+        return processRequest();
+
+    }
+
+    @RequestMapping(path = "/asyncDeferred", method = RequestMethod.GET)
+    public DeferredResult<String> getValueAsyncUsingDeferredResult() {
+
+        log.info("Request received");
+
+        DeferredResult<String> stringDeferredResult = new DeferredResult<>();
+
+        ExecutorService executorService = Executors.newFixedThreadPool(1);
+        executorService.submit(() -> stringDeferredResult.setResult(processRequest()));
+
+        log.info("Servlet thread released");
+
+        return stringDeferredResult;
+
+    }
+
+	@RequestMapping(path = "/asyncCompletable", method = RequestMethod.GET)
+	public CompletableFuture<String> getValueAsyncUsingCompletableFuture() {
+
+	    log.info("Request received");
+
+        CompletableFuture<String> stringCompletableFuture = CompletableFuture
+                .supplyAsync(this::processRequest);
+
+        log.info("Servlet thread released");
+
+		return stringCompletableFuture;
+
+	}
+
 }
--- src/test/java/com/nickebbitt/ApplicationTests.java
+++ src/test/java/com/nickebbitt/ApplicationTests.java
@@ -1,5 +1,6 @@
 package com.nickebbitt;
 
+import lombok.extern.slf4j.Slf4j;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -7,22 +8,53 @@ import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.web.client.TestRestTemplate;
 import org.springframework.test.context.junit4.SpringRunner;
 
-import static com.nickebbitt.Application.ASYNC_RESULT;
+import static com.nickebbitt.Application.RESULT;
 import static org.assertj.core.api.Assertions.assertThat;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+@Slf4j
 public class ApplicationTests {
 
 	@Autowired
 	private TestRestTemplate restTemplate;
 
 	@Test
-	public void asyncEndpointShouldReturnString() {
+	public void syncEndpointShouldReturnString() {
 
-		final String result = restTemplate.getForObject("/async", String.class);
+		log.info("Before request");
 
-		assertThat(result).isEqualTo(ASYNC_RESULT);
+		final String result = restTemplate.getForObject("/sync", String.class);
+
+		log.info("After request");
+
+		assertThat(result).isEqualTo(RESULT);
+
+	}
+
+    @Test
+    public void asyncEndpointShouldReturnStringWithDeferredResult() {
+
+		log.info("Before request");
+
+        final String result = restTemplate.getForObject("/asyncDeferred", String.class);
+
+		log.info("After request");
+
+        assertThat(result).isEqualTo(RESULT);
+
+    }
+
+	@Test
+	public void asyncEndpointShouldReturnStringWithCompletableFuture() {
+
+		log.info("Before request");
+
+		final String result = restTemplate.getForObject("/asyncCompletable", String.class);
+
+		log.info("After request");
+
+		assertThat(result).isEqualTo(RESULT);
 
 	}
 
